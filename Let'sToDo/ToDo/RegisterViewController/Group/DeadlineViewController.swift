ef0c45a737021623780a1ce0119155f8a69829cc
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-07-09 22:38:47 +0900

    마감일 레이블 추가 과제 완료, BaseView연결로 가독성 높임

--- Let'sToDo/ToDo/RegisterViewController/Group/DeadlineViewController.swift
+++ Let'sToDo/ToDo/RegisterViewController/Group/DeadlineViewController.swift
@@ -8,9 +8,8 @@
 import UIKit
 import SnapKit
 
-class DeadlineViewController: UIViewController {
+class DeadlineViewController: BaseViewController {
     var delegate: DataDelegate?
-    
     var selectedDate: Observable<Void?> = Observable(nil)
     private var date: Date?
     
@@ -24,14 +23,22 @@ class DeadlineViewController: UIViewController {
         picker.clipsToBounds = true
         return picker
     }()
-
+    
     private let selectedDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "선택된 마감일 없음"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
             let label = UILabel()
-            label.text = "선택된 마감일 없음"
+            label.text = "마감일을 선택하세요"
+            label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+            label.textColor = .black
             label.textAlignment = .center
             return label
         }()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,74 +46,78 @@ class DeadlineViewController: UIViewController {
         setupNavigationBar()
         bindData()
     }
-
+    
+    
     private func setupUI() {
         view.backgroundColor = .white
-
-        let titleLabel = UILabel()
-        titleLabel.text = "마감일을 선택하세요"
-        titleLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        titleLabel.textColor = .black
-        titleLabel.textAlignment = .center
-        view.addSubview(titleLabel)
-
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
-            make.leading.trailing.equalToSuperview().inset(20)
-        }
-
-        view.addSubview(datePicker)
-        
-        datePicker.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(20)
-            make.horizontalEdges.equalTo(titleLabel)
-        }
-        
-        view.addSubview(selectedDateLabel)
-                selectedDateLabel.snp.makeConstraints { make in
-                    make.top.equalTo(datePicker.snp.bottom).offset(20)
-                    make.leading.trailing.equalTo(titleLabel)
-                }
-
+        setupHierarchy()
+        setupConstraints()
+     
     }
-
+    
+    override func setupHierarchy() {
+        //서브뷰 추가
+        view.addSubview(titleLabel)
+        view.addSubview(datePicker)
+        view.addSubview(selectedDateLabel)
+    }
+    
+    override func setupConstraints() {
+            // 제약 조건 추가
+            titleLabel.snp.makeConstraints { make in
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+                make.left.right.equalToSuperview().inset(20)
+            }
+            
+            datePicker.snp.makeConstraints { make in
+                make.top.equalTo(titleLabel.snp.bottom).offset(10)
+                make.centerX.equalTo(view)
+                make.width.equalToSuperview().multipliedBy(0.8)
+            }
+            
+            selectedDateLabel.snp.makeConstraints { make in
+                make.top.equalTo(datePicker.snp.bottom).offset(20)
+                make.centerX.equalTo(view)
+            }
+        }
+    
     private func setupNavigationBar() {
         let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backButtonTapped))
         navigationItem.leftBarButtonItem = backButton
     }
-
+    
     
     private func bindData() {
-            selectedDate.bind { [weak self] _ in
-                guard let self = self else { return }
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateStyle = .medium
-                dateFormatter.timeStyle = .none
-                if let date = self.date {
-                    self.selectedDateLabel.text = "선택된 마감일: \(dateFormatter.string(from: date))"
-                } else {
-                    self.selectedDateLabel.text = "선택된 마감일 없음(옵져버 활용)"
-                }
+        selectedDate.bind { [weak self] _ in
+            guard let self = self else { return }
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .none
+            if let date = self.date {
+                self.selectedDateLabel.text = "선택된 마감일: \(dateFormatter.string(from: date))"
+            } else {
+                self.selectedDateLabel.text = "선택된 마감일 없음(옵져버 활용)"
             }
-            
-            datePicker.addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
         }
-
-        @objc private func datePickerValueChanged() {
-            date = datePicker.date
-            selectedDate.value = ()
-        }
+        
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
+    }
     
-
+    @objc private func datePickerValueChanged() {
+        date = datePicker.date
+        selectedDate.value = ()
+    }
+    
+    
     @objc private func backButtonTapped() {
-            if let date = date {
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateStyle = .medium
-                dateFormatter.timeStyle = .none
-                let dateString = dateFormatter.string(from: date)
-                delegate?.passData(dateString, type: .deadline)
-                print("선택한 마감일: \(dateString)")
-            }
-            navigationController?.popViewController(animated: true)
+        if let date = date {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .none
+            let dateString = dateFormatter.string(from: date)
+            delegate?.passData(dateString, type: .deadline)
+            print("선택한 마감일: \(dateString)")
         }
+        navigationController?.popViewController(animated: true)
+    }
 }
